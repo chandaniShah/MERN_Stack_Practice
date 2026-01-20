f0b02a17157aeeab79c06421ffc1da87d86fa5e5
chandaniShah <88714584+chandaniShah@users.noreply.github.com>
2026-01-20 23:35:05 +0530

    Basic loop questions

--- JS/Questions/index.js
+++ JS/Questions/index.js
@@ -82,7 +82,7 @@ function sumArray1(arr) {
   return sum;
 }
 
-console.log(sumArray1([1, 2, 3, 4, 5])); 
+// console.log(sumArray1([1, 2, 3, 4, 5])); 
 
 // write a function to find the factorial of a number
 function factorial(n) {
@@ -91,5 +91,64 @@ function factorial(n) {
   return n * factorial(n - 1);
 }
 
-console.log(factorial(5)); 
-console.log(factorial(0));
+// console.log(factorial(5)); 
+// console.log(factorial(0));
+
+
+// for loop, break, continue,  can we write for loop without the initialization and conditions.
+// while loop -
+// do while loop - feature/fault : the first iteration is guranteed as no condition is checked. At-least run one time.
+// strings -  sequence of chars
+// template string - ``
+// difference btw creating string as a variable and as new String("Hello");
+// 
+
+// print numbers from 1 to 50 using for loop
+
+// for (let i = 1; i <= 50; i++) {
+//   console.log(i);
+// }
+
+// print even numbers from 1 to 100 using for loop
+
+// for (let i = 2; i <= 100; i += 2) {
+//   console.log(i);
+// }
+
+// print numbers from 100 to 1 in reverse order using for loop
+
+// for (let i = 100; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// print multiplication table of a given number using for loop
+
+// const num = 5;
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${num} x ${i} = ${num * i}`);
+// }
+
+// print the odd numbers from an array using for loop
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 !== 0) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// calculate the sum of all numbers in an array using for loop
+
+let sum = 0;     
+for (let i = 0; i < arr.length; i++) {
+  sum += arr[i];
+}
+// console.log("Sum:", sum);
+
+// Print the squares of numbers from 1 to 10.
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`Square of ${i} is ${i * i}`);
+} 
+
